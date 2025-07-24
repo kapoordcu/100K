1ad905ee03c1567315a135f76c9de665e4b4f376
gauravkapoor <gaurav.kapoor23@outlook.com>
2025-07-24 23:17:41 +0530

    mathematics 1

--- src/main/java/gk/practic/MathematicalQ.java
+++ src/main/java/gk/practic/MathematicalQ.java
@@ -2,6 +2,7 @@ package gk.practic;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class MathematicalQ {
@@ -21,6 +22,47 @@ public class MathematicalQ {
            dp[i] = dp[i-1]*i;
         }
         return dp[n];
+    }
+
+    public int numberOfTrailingZeroesInFactorial(int n) {
+        int res = 0;
+        for (int i = 5; i <= n; i*=5) {
+            res += n/i;
+        }
+        return res;
+    }
+
+    public boolean palindromeOrNotFirst(int n) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(n);
+        String v1 = sb.toString();
+        String v2 = sb.reverse().toString();
+        return v1.equals(v2);
+    }
+
+    public boolean palindromeOrNot2(int n) {
+        int originalnumber = n;
+        int reverse = 0;
+        while (n % 10 != 0) {
+            reverse = reverse * 10 + n % 10;
+            n = n / 10;
+        }
+        return reverse == originalnumber;
+    }
+
+    @Test
+    public void testpalindromeOrNot() {
+        assertTrue(palindromeOrNot2(121));
+        assertFalse(palindromeOrNot2(13));
+        assertTrue(palindromeOrNot2(3));
+    }
+
+    @Test
+    public void testnumberOfTrailingZeroesInFactorial() {
+        int tw = numberOfTrailingZeroesInFactorial(12);
+        assertTrue(tw==2);
+        tw = numberOfTrailingZeroesInFactorial(26);
+        assertTrue(tw==6);
     }
 
     @Test
