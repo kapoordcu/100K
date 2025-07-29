af97dcb5aabd69cb84a51d02282d99b5f82df0f8
gauravkapoor <gaurav.kapoor23@outlook.com>
2025-07-29 23:09:00 +0530

    kadane

--- src/main/java/gk/practic/Arrays.java
+++ src/main/java/gk/practic/Arrays.java
@@ -9,14 +9,66 @@ import static org.junit.Assert.assertTrue;
 
 public class Arrays {
 
+    public int kadaneOn(int[] arr) {
+        //  {-5, 4,6,-3, 4,-1};
+        int maxSum = 0;
+        int currSum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            currSum += arr[i];
+            if(currSum > maxSum) {
+                maxSum = currSum;
+            }
+            if(currSum < 0) {
+                currSum = 0;
+            }
+        }
+        return maxSum;
+    }
+    public int kadaneBR(int[] arr) {
+        int sum = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                sum = Math.max(sum, sumupto(arr, i, j));
+            }
+        }
+        return sum;
+    }
+
+    private int sumupto(int[] arr, int i, int j) {
+       int sum = 0;
+        for (int k = i; k <= j; k++) {
+            sum += arr[k];
+        }
+        return sum;
+    }
+
     public int mooreVotingAlgorithm(int[] arr) {
-        return arr[arr.length-1];
+        int element = arr[0];
+        int count = 0;
+        int counter = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if(count==0) {
+                count = 1;
+                element = arr[i];
+            } else if(arr[i] != element) {
+                count--;
+            } else {
+                count++;
+            }
+
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]==element) {
+                counter++;
+            }
+        }
+        return counter > arr.length/2 ? element : -1;
     }
 
     public int bfUsingMap(int[] arr) {
         Map<Integer, Integer> freq = new HashMap<>();
-        for (int element: arr) {
-            freq.put(element, freq.getOrDefault(element, 0) + 1);
+        for (int i = 0; i < arr.length; i++) {
+            freq.put(arr[i], freq.getOrDefault(arr[i], 0) + 1);
         }
         return freq.entrySet()
                 .stream().max(Map.Entry.comparingByValue())
@@ -47,7 +99,18 @@ public class Arrays {
         int[] arr2 = {5, 4,4,1,1};
         assertTrue(bf(arr)==1);
         assertTrue(bfUsingMap(arr)==1);
+        assertTrue(mooreVotingAlgorithm(arr)==1);
         assertTrue(bf(arr2)==-1);
         assertTrue(bfUsingMap(arr2)==-1);
+        assertTrue(mooreVotingAlgorithm(arr2)==-1);
+    }
+
+    @Test
+    public void testKadane() {
+        int[] arr = {-5, 4,6,-3, 4,-1};
+        int[] arr2 = {5, -4,-2,6,-1};
+        assertTrue(kadaneBR(arr)==11);
+       // assertTrue(kadaneOn(arr)==11);
+        assertTrue(kadaneOn(arr2)==6);
     }
 }
