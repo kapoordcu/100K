a9b868cf38ec9cade4c7d8cf478f40fadcb65b2f
gaurakap <gaurav.ga.kapoor@oracle.com>
2026-02-01 19:59:36 +0100

    subarray sum solution added

--- src/main/java/gk/practic/prefixsum/Application.java
+++ src/main/java/gk/practic/prefixsum/Application.java
@@ -1,6 +1,26 @@
 package gk.practic.prefixsum;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Application {
+
+    public int subarraySum(int[] nums, int k) {
+        // 2, 3, 5, -5, -5, 1, 4
+        int total = 0;
+        int sum = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (map.containsKey(sum - k)) {
+                total += map.get(sum - k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return total;
+    }
+
     public int pivotIndex(int[] nums) {
         int total = 0;
         for (int i = 0; i < nums.length; i++) {
@@ -23,5 +43,8 @@ public class Application {
         Application prefixSumApp = new Application();
         int[] nums = {1,7,3,6,5,6};
         System.out.println(prefixSumApp.pivotIndex(nums)); // 3
+        Application subarraySumApp = new Application();
+        int[] nums2 = {2, 3, 5, -5, -5, 1, 4};
+        System.out.println(subarraySumApp.subarraySum(nums2, 5));
     }
 }
