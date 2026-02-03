fcadcd8e68bda2fa12527b1f78ff5bdb426ca328
kapoordcu <gaurav.kapoor23@outlook.com>
2026-02-03 20:47:33 +0100

    checkSubarraySum

--- src/main/java/gk/practic/prefixsum/Application.java
+++ src/main/java/gk/practic/prefixsum/Application.java
@@ -39,6 +39,24 @@ public class Application {
         return -1;
     }
 
+    public boolean checkSubarraySum(int[] nums, int k) {
+        int sum  = 0;
+        Map<Integer, Integer> remainderMap = new HashMap<>();
+        remainderMap.put(0, -1);
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            int remainder = sum % k;
+            if(remainderMap.containsKey(remainder)) {
+                if(i-remainderMap.get(remainder) > 1) {
+                    return true;
+                }
+            } else {
+                remainderMap.put(remainder, i);
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         Application prefixSumApp = new Application();
         int[] nums = {1,7,3,6,5,6};
@@ -46,5 +64,9 @@ public class Application {
         Application subarraySumApp = new Application();
         int[] nums2 = {2, 3, 5, -5, -5, 1, 4};
         System.out.println(subarraySumApp.subarraySum(nums2, 5));
+        Application subArraySum = new Application();
+        int[] nums3 = {23,2,4,6,7};
+        int k = 6;
+        System.out.println(subArraySum.checkSubarraySum(nums3, k));
     }
 }
