cd0e3996a7f46d6ca35985efd741602a0ca3ccef
kapoordcu <gaurav.kapoor23@outlook.com>
2026-02-06 23:15:25 +0100

    insertionSort

--- src/main/java/gk/practic/sorting/ApplicationSorting.java
+++ src/main/java/gk/practic/sorting/ApplicationSorting.java
@@ -3,43 +3,66 @@ package gk.practic.sorting;
 public class ApplicationSorting {
     public static void main(String[] args) {
         ApplicationSorting sorting = new ApplicationSorting();
-        int[] unsortedArray = {6, 2, 5, 7};
-        //sorting.bubbleSort(unsortedArray);
-        sorting.selectionSort(unsortedArray);
-        System.out.println();
+        int[] unsortedArray = {12, 11, 13, 5, 6};
+
+        sorting.insertionSort(unsortedArray);
+        for (Integer i: unsortedArray) {
+            System.out.print(i + " ");
+        }
     }
 
-    private void bubbleSort(int[] unsortedArray) {
-        int len = unsortedArray.length;
-        boolean swapped = false;
-        for (int outer = 0; outer < len-1; outer++) {
-            for (int inner = 0; inner < len - 1 - outer; inner++) {
-                if(unsortedArray[inner] > unsortedArray[inner + 1]) {
-                    int temp = unsortedArray[inner];
-                    unsortedArray[inner] = unsortedArray[inner+1];
-                    unsortedArray[inner+1] = temp;
-                    swapped = true;
+    private void bubbleSort(int[] arr) {
+        int len = arr.length;
+        boolean swap = false;
+        for (int i = 0; i < len-1; i++) {
+            for (int j = 0; j < len - 1 - i; j++) {
+                if(arr[j + 1] < arr[j]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    swap = true;
                 }
             }
-            if(!swapped) {
+            if(!swap) {
                 break;
             }
         }
     }
 
-    private void selectionSort(int[] array) {
-        int len = array.length;
-        int minIndex = 0;
-        for (int i = 0; i < len-1; i++) {
-            minIndex = i;
-            for (int j = i + 1; j < len; j++) {
-                if(array[j] < array[minIndex]) {
-                    minIndex = j;
+    //{12, 11, 13, 5, 6};
+    private void insertionSort(int[] arr) {
+        int len = arr.length;
+        for (int i = 1; i < len; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >=0 && arr[j] > key) {
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    //{12, 11, 13, 5, 6};
+    private void selectionSort(int[] arr) {
+        int len = arr.length;
+        int minIndx = 0;
+        for (int i = 0; i < len; i++) {
+            minIndx = i;
+            for (int j = i+1; j < len; j++) {
+                if(arr[j] < arr[minIndx]) {
+                    minIndx = j;
                 }
             }
-            int temp = array[minIndex];
-            array[minIndex] = array[i];
-            array[i] = temp;
+            int temp = arr[i];
+            arr[i] = arr[minIndx];
+            arr[minIndx] = temp;
         }
     }
 
