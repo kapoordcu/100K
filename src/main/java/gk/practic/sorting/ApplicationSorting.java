fd77f7fca8e07b7dabdf960a2806d47c72775071
gaurakap <gaurav.ga.kapoor@oracle.com>
2026-02-07 22:28:54 +0100

    merge sort implementation

--- src/main/java/gk/practic/sorting/ApplicationSorting.java
+++ src/main/java/gk/practic/sorting/ApplicationSorting.java
@@ -3,12 +3,60 @@ package gk.practic.sorting;
 public class ApplicationSorting {
     public static void main(String[] args) {
         ApplicationSorting sorting = new ApplicationSorting();
-        int[] unsortedArray = {12, 11, 13, 5, 6};
+        int[] arr = {12, 11, 13, 5, 6};
 
-        sorting.insertionSort(unsortedArray);
-        for (Integer i: unsortedArray) {
+        sorting.mergeSort(arr, 0, arr.length - 1);
+        for (Integer i: arr) {
             System.out.print(i + " ");
         }
+    }
+
+
+    private void mergeSort(int[] arr, int left, int right) {
+        if(left < right) {
+            int mid = left + (right-left)/2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid+1, right);
+            mergeAfterDivision(arr, left, mid, right);
+        }
+    }
+
+    private void mergeAfterDivision(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+        for (int li = 0; li < n1; li++) {
+            L[li] = arr[li+left];
+        }
+        for (int ri = 0; ri < n2; ri++) {
+            R[ri] = arr[mid+1 +ri];
+        }
+        int i = 0;
+        int j = 0;
+        int k = left;
+        while (i < n1 && j < n2) {
+            if(L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+                k++;
+            } else {
+                arr[k] = R[j];
+                j++;
+                k++;
+            }
+        }
+        while(i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+        while(j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+
     }
 
     private void bubbleSort(int[] arr) {
