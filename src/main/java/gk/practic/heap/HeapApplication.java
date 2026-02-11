ca3f95ec15d9097ce22bfee759b260b4481b455f
kapoordcu <gaurav.kapoor23@outlook.com>
2026-02-11 20:54:21 +0100

    remove and add in heap is implemented

--- src/main/java/gk/practic/heap/HeapApplication.java
+++ src/main/java/gk/practic/heap/HeapApplication.java
@@ -4,54 +4,71 @@ import java.util.Arrays;
 
 public class HeapApplication {
 
-        /*
-                   30
-
-             23          19
-
-          18   20      16    24
-
-       9    11
-         */
-
     public static void main(String[] args) {
+        int[] arr = {24, 16, 18, 11, 14, 15, 10};
         HeapApplication application = new HeapApplication();
-
-        int[] arr = {30, 23, 19, 18, 20, 16, 24, 9, 11};
-        int[] newArray = application.addElem(arr, 22);
-        for (int v: newArray) {
-            System.out.print(v + " ");
-        }
-        int[] afterRemoveal = application.remove(newArray);
-        System.out.println("----------");
-        for (int v: afterRemoveal) {
-            System.out.print(v + " ");
-        }
-
+        int[] a = application.insertInMaxHeap(arr,20);
+       for (Integer s: a) {
+           System.out.print(s + " ");
+       }
+       application.removeFromMaxHeap(a);
     }
 
-    private int[] remove(int[] arr) {
-        // 30 23 19 18 22 16 24 9 11 20
-        int[] ret = Arrays.copyOf(arr, arr.length -1);
-        ret[0] = arr[arr.length-1];
-        for (int i = 1; i < arr.length - 1; i++) {
-            ret[i] = arr[i];
+    public int[] insertInMaxHeap(int[] arr, int val) {
+        int[] newArray;
+        if(arr == null) {
+            arr = new int[1];
+            arr[0] = val;
+            return arr;
+        } else {
+            newArray = new int[arr.length + 1];
+            for (int i = 0; i < arr.length; i++) {
+                newArray[i] = arr[i];
+            }
+            newArray[arr.length] = val;
+            int i = newArray.length - 1;
+            while (i > 0) {
+                int parent = (i-1)/2;
+                if(newArray[parent] < newArray[i]) {
+                    swapArrayNumbers(newArray, i, parent);
+                    i = parent;
+                } else {
+                    break;
+                }
+            }
         }
+        return newArray;
+    }
+
+    public int[] removeFromMaxHeap(int[] arr) {
+        int[] newA = new int[arr.length-1];
+        for (int i = 0; i < arr.length - 2; i++) {
+            newA[i+1] = arr[i + 1];
+        }
+        newA[0] = arr[arr.length -1];
         int i = 0;
-        while (i < ret.length) {
-            //  left and right child
-            if(2*i+1<ret.length && 2*i+2<ret.length) {
-                int max = ret[2*i+1] > ret[2*i+2] ? 2*i+1 : 2*i+2;
-                if(ret[i] < ret[max]) {
-                    swapNumbers(ret, i, max);
-                    i = max;
+
+        int len = newA.length;
+        int larger = 0;
+        while (i < len) {
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+            if(left < len && right < len) {
+                if(newA[left] < newA[right]) {
+                    larger = 2 * i + 2;
+                } else {
+                    larger = 2 * i + 1;
+                }
+                if(newA[i] < newA[larger]) {
+                    swapArrayNumbers(newA, i, larger);
+                    i = larger;
                 } else {
                     break;
                 }
-            } else if(2*i+1<ret.length) {
-                if(ret[i] < ret[2*i+1]) {
-                    swapNumbers(ret, i, 2*i+1);
-                    i = 2*i+1;
+            } else if(left < len) {
+                if(newA[i] < newA[left]) {
+                    swapArrayNumbers(newA, i, left);
+                    i = left;
                 } else {
                     break;
                 }
@@ -59,34 +76,12 @@ public class HeapApplication {
                 break;
             }
         }
-        return ret;
+        return arr;
     }
 
-    public  int[] addElem(int[] array, int val) {
-        // {30, 23, 19, 18, 20, 16, 24, 9, 11}; // 9 size
-        int[] ret = new int[array.length+1]; //  10 size
-        for (int j = 0; j < array.length ; j++) { // 9
-            ret[j] = array[j];
-        }
-        ret[array.length] = val;
-        // {30, 23, 19, 18, 20, 16, 24, 9, 11, 22};
-        int i = array.length;
-        while (i >= 0) {
-            int parent = (i-1)/2;
-            if(ret[i] > ret[parent]) {
-                swapNumbers(ret, i, parent);
-                i = parent;
-            } else {
-                break;
-            }
-        }
-        return ret;
+    private void swapArrayNumbers(int[] newArray, int child, int parent) {
+        int temp = newArray[child];
+        newArray[child] = newArray[parent];
+        newArray[parent] = temp;
     }
-
-    private void swapNumbers(int[] ret, int child, int parent) {
-        int temp = ret[child];
-        ret[child] = ret[parent];
-        ret[parent] = temp;
-    }
-
 }
