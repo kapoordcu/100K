cd0aa0017592e16b7cf8922e64477d3d0f80e9cd
gaurakap <gaurav.ga.kapoor@oracle.com>
2026-02-23 23:20:29 +0100

    minPathSumTopDownRecursive space optimization

--- src/main/java/gk/practic/dp/DynamicApp.java
+++ src/main/java/gk/practic/dp/DynamicApp.java
@@ -14,7 +14,27 @@ public class DynamicApp {
 //        System.out.println(dynamicApp.rob(nums));
        // System.out.println(dynamicApp.rob(nums));
         int[][] grid = {{1,3,1},{1,5,1},{4,2,1}};
-        System.out.println(dynamicApp.minPathSumTopDownRecursive(grid));
+        System.out.println(dynamicApp.minPathSum(grid));
+    }
+
+
+    public int minPathSum(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        for (int i = 1; i < rows; i++) {
+            grid[i][0] += grid[i-1][0];
+        }
+        for (int i = 1; i < cols; i++) {
+            grid[0][i] += grid[0][i-1];
+        }
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                int top = grid[i-1][j];;
+                int left = grid[i][j-1];
+                grid[i][j] = Math.min(top, left) + grid[i][j];
+            }
+        }
+        return grid[rows-1][cols-1];
     }
 
     public int minPathSumTopDownRecursive(int[][] grid) {
