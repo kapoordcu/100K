2fd38feb25e844d3ddd83916326e154f70f1bd57
gaurakap <gaurav.ga.kapoor@oracle.com>
2026-02-23 22:13:31 +0100

    minPathSumTopDownRecursive

--- src/main/java/gk/practic/dp/DynamicApp.java
+++ src/main/java/gk/practic/dp/DynamicApp.java
@@ -5,14 +5,45 @@ import java.util.Arrays;
 public class DynamicApp {
 
     public static void main(String[] args) {
-        int value =7;
-        int[] dp = new int[value+1];
-        Arrays.fill(dp,-1);
+//        int value =7;
+//        int[] dp = new int[value+1];
+//        Arrays.fill(dp,-1);
         DynamicApp dynamicApp = new DynamicApp();
         //System.out.println(dynamicApp.fibonacciBottomsUpSpaceOptimization(value));
-        int[] nums = {2,1,1,2};
-        System.out.println(dynamicApp.rob(nums));
+//        int[] nums = {2,1,1,2};
+//        System.out.println(dynamicApp.rob(nums));
        // System.out.println(dynamicApp.rob(nums));
+        int[][] grid = {{1,3,1},{1,5,1},{4,2,1}};
+        System.out.println(dynamicApp.minPathSumTopDownRecursive(grid));
+    }
+
+    public int minPathSumTopDownRecursive(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < dp.length; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        for (int i = m-1; i >=0 ; i--) {
+            for (int j = n-1; j >=0 ; j--) {
+                calculateMinPathSum(grid, i, j, dp);
+            }
+        }
+        return dp[m-1][n-1];
+    }
+
+    private int calculateMinPathSum(int[][] grid, int i, int j, int[][] dp) {
+        if( i< 0 || j < 0) { return Integer.MAX_VALUE;}
+        if(i == 0 && j == 0) {
+            dp[0][0] = grid[0][0];
+            return grid[0][0];
+        }
+        if(dp[i][j] != -1) return dp[i][j];
+        int top = calculateMinPathSum(grid, i-1, j, dp);
+        int left = calculateMinPathSum(grid, i, j-1, dp);
+        dp[i][j] = grid[i][j] + Math.min(top, left);
+        return dp[i][j];
     }
 
     public int fibonacciTopDown(int[] dp, int n) {
