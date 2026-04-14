71940c57553128cda85a3b489ecf5c6e6285a7ce
kapoordcu <gaurav.kapoor23@outlook.com>
2026-04-14 21:59:12 +0530

    maze run reviisted

--- src/main/java/gk/practic/backtrack/BackTrackApp.java
+++ src/main/java/gk/practic/backtrack/BackTrackApp.java
@@ -1,26 +1,30 @@
 package gk.practic.backtrack;
 
+import java.awt.image.VolatileImage;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class BackTrackApp {
     public static void main(String[] args) {
-//        int[] numbers = {1, 2, 3};
+        int[] numbers = {1, 2, 3};
 //        List<Integer> curr = new ArrayList<>();
 //        List<List<Integer>> resilt = new ArrayList<>();
         BackTrackApp app = new BackTrackApp();
+//        List<List<Integer>> allSubsetUsingBacktrack = app.getAllSubsetUsingBacktrackWithBitmask(numbers);
+//        allSubsetUsingBacktrack.forEach(System.out::println);
 //        app.backtrackGeneric(0, numbers, curr, resilt);
-        List<List<String>> lists = app.solveNQueens(4);
+        //List<List<String>> lists = app.solveNQueens(4);
 //        System.out.println(resilt);
-//        int maze[][] = {
-//                {1, 0, 0, 0},
-//                {1, 1, 0, 1},
-//                {1, 1, 0, 0},
-//                {0, 1, 1, 1}
-//        };
-//
-//        List<String> allPaths = app.mazeRun(maze);
-//        System.out.println(allPaths);
+        int maze[][] = {
+                {1, 0, 0, 0},
+                {1, 1, 0, 1},
+                {1, 1, 0, 0},
+                {0, 1, 1, 1}
+        };
+
+        List<String> allPaths = app.mazeRun(maze);
+        System.out.println(allPaths);
     }
 
     public List<List<String>> solveNQueens(int n) {
@@ -44,17 +48,37 @@ public class BackTrackApp {
     }
 
     private List<String> mazeRun(int[][] maze) {
-        int n =  maze.length;
-        boolean[][] visited = new boolean[n][n];
-        int[][] directions = {
-                {1, 0, 'D'},
-                {-1, 0, 'U'},
-                {0, 1, 'R'},
-                {0, -1, 'L'}
-        };
+        int size = maze.length;
         List<String> result = new ArrayList<>();
-        backTrackMaze(0, 0, n, maze, visited, directions, "", result);
+        boolean[][] visited = new boolean[size][size];
+        if(maze[0][0] == 1) {
+            backtrackMazeRev(0, 0, size, maze, "", visited, result);
+        }
         return result;
+    }
+
+    private void backtrackMazeRev(int x, int y, int size, int[][] maze, String path, boolean[][] visited, List<String> result) {
+        int[][] directions = {
+                {-1, 0, 'L'},     //left
+                {1, 0, 'R'},     // right
+                {0, -1, 'U'},     // top
+                {0, 1, 'D'}      //down
+        };
+        if(x==size-1 && y==size-1) {
+            result.add(path);
+            return;
+        }
+        visited[x][y]= true;
+        for (int[] dir: directions) {
+            int newX = x + dir[0];
+            int newY = y + dir[1];
+            char di = (char)dir[2];
+            if(newX>=0 && newX<size && newY>=0 && newY<size &&
+                    !visited[newX][newY] && maze[newX][newY]==1) {
+                backtrackMazeRev(newX, newY, size, maze, path+di, visited, result);
+            }
+        }
+        visited[x][y]= false;
     }
 
     private void backTrackMaze(int x, int y, int n, int[][] maze, boolean[][] visited, int[][] directions, String path, List<String> result) {
@@ -82,6 +106,37 @@ public class BackTrackApp {
             curr.add(nums[i]);
             backtrackGeneric(i + 1, nums, curr, resilt);
             curr.remove(curr.size() - 1);
+        }
+    }
+
+    public List<List<Integer>> getAllSubsetUsingBacktrack(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtracksubset(nums, 0, result, new ArrayList<>());
+        return result;
+    }
+
+    public List<List<Integer>> getAllSubsetUsingBacktrackWithBitmask(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        int n = nums.length; //3
+        int sets = 1 << n; // 2"3 = 8
+        for (int mask = 0; mask < sets; mask++) {
+            List<Integer> subset = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                if((mask & (1 << i)) != 0) {
+                    subset.add(nums[i]);
+                }
+            }
+            result.add(subset);
+        }
+        return result;
+    }
+
+    private void backtracksubset(int[] nums, int index, List<List<Integer>> result, ArrayList<Integer> current) {
+        result.add(new ArrayList<>(current));
+        for (int i = index; i < nums.length; i++) {
+            current.add(nums[i]);
+            backtracksubset(nums, i+1, result, current);
+            current.remove(current.size()-1);
         }
     }
 }
